4029a26cd2e3359db03da7a3b2ae36fffc41d461
KK <k.koichi276052@gmail.com>
2024-02-07 06:53:31 +0900

    checkpoint

--- complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -74,52 +74,109 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-const displayMovements = function (movements) {
-  movements.forEach(function (movement, i) {
-    const type = movement > 0 ? 'deposit' : 'withdrawal';
+const displayMovements = function (movements, sort = false) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const content = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${movement}€</div>
-      </div>
-    `;
+          <div class="movements__value">${mov}€</div>
+        </div>
+      `;
 
     containerMovements.insertAdjacentHTML('afterbegin', content);
   });
 };
 
-/////////////////////////////////////////////////////////////////////////////
-// Login function(check credentials)
-const checkCredentials = function () {
-  const username = inputLoginUsername.value;
-  const firstName = username.split(' ')[0];
-  const pin = +inputLoginPin.value;
-  console.log(firstName, pin);
-
-  // accounts.forEach((account, i) => {
-  //   if (account.owner === username && pin === account.pin) {
-  //     labelWelcome.innerText = `Hello ${firstName}!`;
-  //     containerApp.style.opacity = '1';
-  //     displayMovements();
-  //     return;
-  //   } else {
-  //   }
-  // });
-
-  for (const account of accounts) {
-    console.log(account.owner === username);
-    console.log(pin === account.pin);
-    if (account.owner === username && pin === account.pin) {
-      labelWelcome.innerText = `Hello ${firstName}!`;
-      containerApp.style.opacity = '1';
-      displayMovements(account.movements);
-      return;
-    } else {
-    }
-  }
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}$`;
 };
 
-btnLogin.addEventListener('click', checkCredentials);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}$`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}$`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
+/////////////////////////////////////////////////////////////////////////////
+// Login function(check credentials) my own one
+// const checkCredentials = function () {
+//   // const username = inputLoginUsername.value;
+//   // const firstName = username.split(' ')[0];
+//   // const pin = +inputLoginPin.value;
+
+//   // for (const account of accounts) {
+//   //   if (account.owner === username && pin === account.pin) {
+//   //     labelWelcome.innerText = `Hello ${firstName}!`;
+//   //     containerApp.style.opacity = '1';
+//   //     displayMovements(account.movements);
+//   //     return;
+//   //   } else {
+//   //   }
+//   // }
+
+//   e.preventDefault();
+
+//   if (condition) {
+//   }
+// };
+
+const updateUI = function (acc) {
+  displayMovements(acc.movements);
+  calcDisplayBalance(acc);
+  calcDisplaySummary(acc);
+};
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split('')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    inputLoginPin.value = '';
+    inputLoginUsername.value = '';
+    // blur() remove focus from a text output
+    inputLoginPin.blur();
+
+    updateUI(currentAccount);
+  }
+});
