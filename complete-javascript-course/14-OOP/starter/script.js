d4be99013451fa16d051d3786d5d2eed1d883023
KK <k.koichi276052@gmail.com>
2024-03-04 19:47:39 +0900

    OOP WIP

--- complete-javascript-course/14-OOP/starter/script.js
+++ complete-javascript-course/14-OOP/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// ES6
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+const koichi = new Person('koichi', 1996);
+console.log(koichi);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2053 - this.birthYear);
+};
+
+console.log(Person.prototype);
+
+const john = new Person('john', 1994);
+console.log(john);
+
+john.calcAge();
