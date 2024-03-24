f41b545ff8540d567e8a2febb157d51b53f57d39
KK <k.koichi276052@gmail.com>
2024-03-25 07:19:21 +0900

    top level await

--- complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -4,3 +4,18 @@ ShoppingCart.addToCart('shsehb', 5);
 import add from './shoppingCart.js';
 
 add();
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+// top level await normally synchronous so make it async function
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
