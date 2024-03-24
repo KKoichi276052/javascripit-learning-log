43a8ad61a7d224d06fae4cccab51a75ab34c5deb
KK <k.koichi276052@gmail.com>
2024-03-24 18:25:25 +0900

    code challenge 2

--- complete-javascript-course/16-Asynchronous/starter/script.js
+++ complete-javascript-course/16-Asynchronous/starter/script.js
@@ -98,6 +98,14 @@ const renderError = function (msg) {
 //     });
 // };
 
+const imgContainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -150,9 +158,24 @@ const createImage = function (imgPath) {
   });
 };
 
+let currentImg;
+
 createImage('img/img-1.jpg')
   .then(img => {
+    currentImg = img;
     console.log('Image 1 loaded');
-    wait(2);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
   })
   .catch(err => console.error(err));
