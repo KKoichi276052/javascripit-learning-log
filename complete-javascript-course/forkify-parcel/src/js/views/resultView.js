4d7cc39d0838406e5a2dee0b234a646306b8d34a
KK <k.koichi276052@gmail.com>
2024-04-04 00:24:50 +0900

    finish bookmark

--- complete-javascript-course/forkify-parcel/src/js/views/resultView.js
+++ complete-javascript-course/forkify-parcel/src/js/views/resultView.js
@@ -1,3 +1,4 @@
+import previewView from "./previewView";
 import View from "./View";
 import icons from "url:../../img/icons.svg";
 
@@ -7,29 +8,9 @@ class ResultView extends View {
   _message;
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join("");
-  }
-
-  _generateMarkupPreview(result) {
-    return `
-			<li class="preview">
-				<a class="preview__link" href="#${result.id}">
-					<figure class="preview__fig">
-						<img src="${result.image}" alt="${result.title}" />
-					</figure>
-					<div class="preview__data">
-						<h4 class="preview__title">${result.title}</h4>
-						<p class="preview__publisher">${result.publisher}</p>
-						<div class="preview__user-generated">
-							<svg>
-								<use href="${icons}#icon-user"></use>
-							</svg>
-						</div>
-					</div>
-				</a>
-			</li>
-			
-		`;
+    return this._data
+      .map((result) => previewView.render(result, false))
+      .join("");
   }
 }
 
