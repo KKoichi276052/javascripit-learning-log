9dae8ae1e993e81ce0ec42ff44f973d219cd40c0
KK <k.koichi276052@gmail.com>
2024-04-06 07:00:41 +0900

    gitignore

---
+++ complete-javascript-course/forkify-parcel/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const API_KEY = "28e45b7c-234f-4704-bd5f-db0b30cb0a1a";
+export const TIMEOUT_SEC = 5;
+export const RESULTS_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
